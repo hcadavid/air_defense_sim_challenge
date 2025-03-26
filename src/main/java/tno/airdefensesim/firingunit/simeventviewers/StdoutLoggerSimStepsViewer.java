b71bef32c22cc83dfa2b59675713ad2459b8a42a
Hector Cadavid <hector.cadavid@gmail.com>
2025-03-26 09:45:03 +0100

    Making the output of the log-based steps viewer more consistent

--- src/main/java/tno/airdefensesim/firingunit/simeventviewers/StdoutLoggerSimStepsViewer.java
+++ src/main/java/tno/airdefensesim/firingunit/simeventviewers/StdoutLoggerSimStepsViewer.java
@@ -23,21 +23,21 @@ public class StdoutLoggerSimStepsViewer implements SimulationStepsViewer{
     @Override
     public void handleIncomingRadarPacker(RadarPacket packet, boolean threat) {
         if (threat){
-            logger.info(String.format("\uD83D\uDEA8 Radar packet received with timestamp %d with threat detected : [%s]",packet.getTime(),packet.getContent()));
+            logger.info(String.format("[timestamp:%d] \uD83D\uDEA8 Radar packet received, threat detected : [%s]",packet.getTime(),packet.getContent()));
         }
         else{
-            logger.info(String.format("\u2705 Radar packet received with timestamp %d but no threat detected : [%s]",packet.getTime(),packet.getContent()));
+            logger.info(String.format("[timestamp:%d] \u2705 Radar packet received, no threat detected : [%s]",packet.getTime(),packet.getContent()));
         }
     }
 
     @Override
     public void handleSuccessfullEngagement(RadarPacket packet) {
-        logger.info(String.format("\t \uD83D\uDCA5 Successful engagement [timestamp:%d] with radar packet {%s}",packet.getTime(),packet.getContent()));
+        logger.info(String.format("\t [timestamp:%d] \uD83D\uDCA5 Successful engagement - radar packet {%s}",packet.getTime(),packet.getContent()));
     }
 
     @Override
     public void handleUnsuccessfullEngagement(RadarPacket packet) {
-        logger.info(String.format("\t \u26A0 \u2708 Unsuccessful engagement [timestamp:%d] with radar packet {%s}",packet.getTime(),packet.getContent()));                    
+        logger.info(String.format("\t [timestamp:%d] \u26A0 \u2708 Unsuccessful engagement - radar packet {%s}",packet.getTime(),packet.getContent()));                    
     }
 
     @Override
